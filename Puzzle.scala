64722328aaf3210bb9e3214b8725a84708b0f1e0
Anders <andersmyrmel@gmail.com>
2022-09-16 00:12:22 +0200

    edited commenting

--- Puzzle.scala
+++ Puzzle.scala
@@ -1,11 +1,6 @@
 import puzzlereaderwriter._
 import puzzlesolver._
 
-// Create a puzzle input file, as shown in Canvas (Pages/Description: Unequal)
-// • Read the file, print out the number of puzzles and their sizes
-// • Make a function that “solves” each puzzle by simply replacing each empty square by a random number between 1 and puzzle size.
-// • Create a output file using the above solution(s)
-
 @main def run() = {
     val fileName = "./Unequal.txt";
     val size = Read.getSize(fileName)
